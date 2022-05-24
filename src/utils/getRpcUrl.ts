a01a6b4df0a0bebd0e93112d1c4a89824847944b
SuperBlockchainMan <talentdeveloper59@gmail.com>
2022-05-25 00:05:38 +0900

    Fix node error

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -13,9 +13,6 @@ export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2
 const getNodeUrl = () => {
   // Use custom node if available (both for development and production)
   // However on the testnet it wouldn't work, so if on testnet - comment out the REACT_APP_NODE_PRODUCTION from env file
-  if (process.env.REACT_APP_NODE_PRODUCTION) {
-    return process.env.REACT_APP_NODE_PRODUCTION
-  }
   return sample(nodes)
 }
 
